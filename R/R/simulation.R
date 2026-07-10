0e492bf4584c95bca260b089282bbc669d77edff
Jerson <mohamadjerson@gmail.com>
2026-07-10 15:49:56 +0800

    Add simulation framework

---
+++ R/R/simulation.R
@@ -0,0 +1,25 @@
+#' Monte Carlo Simulation Framework
+#'
+#' Runs Monte Carlo simulations for ridge regression estimators.
+#'
+#' @param n Sample size.
+#' @param p Number of predictors.
+#' @param rho Correlation coefficient.
+#' @param replications Number of simulation runs.
+#'
+#' @return Simulation results.
+#'
+#' @export
+
+simulationStudy <- function(
+    n,
+    p,
+    rho,
+    replications = 1000
+){
+
+    stop(
+        "Simulation engine under development."
+    )
+
+}
