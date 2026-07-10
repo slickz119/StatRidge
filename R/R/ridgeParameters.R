5c34841b36a2d03151bfe51e28e24cde7f432601
Jerson <mohamadjerson@gmail.com>
2026-07-10 15:39:07 +0800

    Add ridge parameter framework

---
+++ R/R/ridgeParameters.R
@@ -0,0 +1,50 @@
+#' Ridge Parameter Estimators
+#'
+#' Computes ridge parameter estimators used in StatRidge.
+#'
+#' @param sigma2 Estimated variance.
+#' @param alpha Eigen-transformed regression coefficients.
+#' @param eigenvalues Eigenvalues of X'X.
+#'
+#' @return A list of ridge parameter estimators.
+#'
+#' @export
+
+ridgeParameters <- function(sigma2, alpha, eigenvalues){
+
+    p <- length(alpha)
+
+    alpha.max <- max(alpha^2)
+
+    k1 <- sigma2 / alpha.max
+
+    k2 <- 1 / alpha.max
+
+    # Remaining estimators to be added
+k3 <- sigma.hat.sq / (prod(alpha.hat^2)^(1/p))
+k4 <- median(sqrt(sigma.hat.sq / alpha.hat^2))
+S <- rep(1, p) 
+for (i in 1:p) {
+  S[i] <- Eigen$values[i] * sigma.hat.sq / ((n - p) * sigma.hat.sq + Eigen$values[i] * (alpha.hat[i])^2) 
+}
+k5 <- max(S)
+k6 <- max(1 / (sqrt(sigma.hat.sq / alpha.hat^2)))
+k7 <- (prod(1 / (sqrt(sigma.hat.sq / alpha.hat^2))))^(1/p)
+k8 <- median(1 / (sqrt(sigma.hat.sq / alpha.hat^2)))
+# Calculate k9 using your formula
+# Example formula: k9 <- mean(x)  # Replace this with your actual formula for k9
+k9 <- 0.4512171333234585*k6+0.269033582616763*k7+0.27974928405978*k8  # Example value; replace this line with your formula
+
+    list(
+        k1 = k1,
+        k2 = k2,
+        k3 = k3,
+        k4 = k4,
+        k5 = k5,
+        k6 = k6,
+        k7 = k7,
+        k8 = k8,
+        k9 = k9
+    )
+
+}
